7252d80727fc11793895a0193b9319a1d347d68f
mohamed-aje <chochoaj93@gmail.com>
2023-03-08 14:17:35 +0200

    added whatgpt3 component and feature and their css

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatGPT3.css";
 const WhatGPT3 = () => {
-  return <div>whatGPT3</div>;
+  return (
+    <div className="whatgpt3 section__margin" id="whatisgpt3">
+      <div className="whatgpt3-feature">
+        <Feature
+          title="Lorem ipsum dolor"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet semper lorem. Fusce molestie risus in maximus fermentum. Mauris auctor in velit sed maximus. Praesent gravida orci eget purus vestibulum consequat"
+        />
+      </div>
+      <div className="whatgpt3-heading">
+        <h1 className="gradiant__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="whatgpt3-container">
+        <Feature
+          title="Lorem  "
+          text="Duis sit amet nunc in dolor tincidunt hendrerit. Sed luctus finibus nibh, sed rutrum augue consequat id. "
+        />
+        <Feature
+          title=" ipsum "
+          text="est sed tincidunt pulvinar, elit elit luctus urna, eu vestibulum urna libero nec lorem "
+        />
+        <Feature
+          title=" dolor"
+          text="Sed tempor enim et dolor tincidunt finibus. Curabitur porttitor porta justo nec aliquam. Vivamus tincidunt eros mi "
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
