a580cf349d4c14d5a5d71543fd9f02cc43bfecb1
mohamed-aje <chochoaj93@gmail.com>
2023-03-09 01:17:45 +0200

    added cta component

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./cta.css";
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="cta" id="cta">
+      <div className="cta-content">
+        <p>sed rutrum augue consequat id</p>
+        <h3>sed rutrum augue consequat id rutrum</h3>
+      </div>{" "}
+      <div className="cta-btn">
+        <button type="submit">Get started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
